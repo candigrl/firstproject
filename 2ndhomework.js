98d4c5ecc7152bd320e4749a2cff14365945945e
candigrl <pgithuka@aol.com>
2020-01-30 12:08:18 -0500

    modified 2ndHomework.js and added JS function.js

--- 2ndhomework.js
+++ 2ndhomework.js
@@ -4,4 +4,7 @@ console.log(grass);
 var number1 = 1;
 var number2 = 2;
 console.log(number1 + number2);
+console.log(number1 * number2);
+var select = true;
+console.log(number2 + select);
 
---
+++ JS function.js
@@ -0,0 +1,42 @@
+var count = 15;
+var doCount = 15;
+
+function print(){
+    while(count > 10){
+        console.log("test");
+        count--;
+   }
+   do{
+       console.log("hiiiii");
+       doCount--;
+   }    
+   while(doCount > 20);    
+      count = 200;
+}
+console.log(count);
+
+function prettyPrint(){
+    for(var i = 0; i<10; i++){
+        console.log("more testing");
+    }
+}
+
+function
+printName(firstName,lastName){
+}
+
+function addThem(a, B){
+    return a * b;
+}
+ 
+functiom myFunction(name) {
+return "Hello " + name;
+}
+
+var result = addThem(1,4);
+console.log(myFunction("morgan"));
+console.log(result);
+
+(function({
+    console.log("fancy");
+})();
