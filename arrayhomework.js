7d4c890fe8ffe16018ba20f617d13e4e10b73116
candigrl <pgithuka@aol.com>
2020-02-03 13:56:05 -0500

    arrayhomework

---
+++ arrayhomework.js
@@ -0,0 +1,8 @@
+var food = ["eggs" , "butter", "cheese" , "bread" , "cream" , "fish" , "pasta" , "spinach" , "apples" , "bananas" ];
+var i;
+for (i = 0; i < food.length; i++) {
+    console.log(food[i]); 
+}
+
+
+
