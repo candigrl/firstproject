05cd8c1b9cfa8bfc9e50ddb4e9fe1c24d94b7910
candigrl <pgithuka@aol.com>
2020-02-03 14:33:44 -0500

    arrayhomework

--- arrayhomework.js
+++ arrayhomework.js
@@ -3,6 +3,9 @@ var i;
 for (i = 0; i < food.length; i++) {
     console.log(food[i]); 
 }
-
-
-
+var groceries = "eggsforbreakfast";
+for (i = 0; i < groceries.length; i++){
+    console.log(groceries[i]);
+    
+}
+ 
